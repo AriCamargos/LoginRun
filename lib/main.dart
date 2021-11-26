b88e3ec0f4482cd9a27a04480454a57bd854c600
Ariana Camargos <86811983+AriCamargos@users.noreply.github.com>
2021-11-25 23:25:34 -0300

    runapp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loginrun_flutter/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,10 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Run App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
       ),
+      home: LoginPage(),
     );
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,116 @@
-import 'package:flutter/material.dart';
-class LoginPage extends StatelessWidget {
+import 'dart:ffi';
 
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        shadowColor: Colors.white,
+        centerTitle: true,
+        title: Text(
+          'Sign In',
+          style: TextStyle(
+              fontFamily: 'RedHatText',
+              fontSize: 15,
+              fontWeight: FontWeight.w900),
+        ),
+        actions: [
+          Icon(
+            Icons.settings_outlined,
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              right: 20,
+            ),
+          )
+        ],
+      ),
+      body: Container(
+        //Borda da logo
+        padding: EdgeInsets.only(
+          top: 60,
+          left: 40,
+          right: 40,
+          bottom: 60,
+        ),
+        color: Colors.white,
+        //Inserindo as listas
+        child: ListView(children: [
+          //Definindo a imagem da logo
+          SizedBox(
+            width: 128,
+            height: 128,
+            child: Image.asset("assets/images/logo.png"),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          TextFormField(
+            autofocus: true,
+            keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(50),
+                borderSide: BorderSide(color: Colors.black26),
+              ),
+              enabledBorder: OutlineInputBorder(borderSide: BorderSide.none),
+              filled: false,
+              hintText: "Username",
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextFormField(
+            autofocus: true,
+            keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(50),
+                borderSide: BorderSide(color: Colors.black26),
+              ),
+              enabledBorder: OutlineInputBorder(borderSide: BorderSide.none),
+              filled: false,
+              hintText: "Password",
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            child: Text(
+              'Login'.toUpperCase(),
+              style: TextStyle(
+                  fontSize: 10,
+                  color: Colors.white,
+                  backgroundColor: Colors.black),
+            ),
+            style: OutlinedButton.styleFrom(
+              backgroundColor: Colors.black,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              'Forgot password?',
+              style: TextStyle(
+                  decoration: TextDecoration.underline,
+                  fontSize: 15,
+                  color: Colors.black38,
+                ),
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
